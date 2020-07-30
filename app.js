e06bd0f40be76f1151c68d81fd090f0112c53d58
sathees-saty <satheeskumar.pmp@gmail.com>
2020-07-30 09:45:03 +0530

    second commit

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
+var http = require("http");
 const config = require('./config');
 
-const express = require('express');
-const app = express();
+http.createServer(function (request, response) {
+   // Send the HTTP header 
+   // HTTP Status: 200 : OK
+   // Content Type: text/plain
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+   
+   // Send the response body as "below"
+   response.end('Welcome to SATY Learning \nBuild, Run, & Continuously Deploy Docker Containers to Azure App Service');
+}).listen(config.port);
 
-app.get('/', (req, res) => {
-  res.send('Welcome to SATY Learning!/nBuild, Run, & Continuously Deploy Docker Containers to Azure App Service');
-  
-});
-
-app.listen(config.port, () => {
-  console.log(`Server started on port ${config.port}`);
-});
+// Console will print the message
+console.log(`Server started on port ${config.port}`);
